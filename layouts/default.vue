335f3244750864b142da184615ddb4a75c1a7f69
smile2gether <vlike2smile@gmail.com>
2019-11-21 19:58:27 +0700

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-app dark>
+    <v-app-bar
+      color="primary"
+      fixed
+      dense
+      app>
+      <v-toolbar-title v-text="title" />
+    </v-app-bar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-navigation-drawer
+      v-model="rightDrawer"
+      :right="right"
+      temporary
+      fixed
+    >
+      <v-list>
+        <v-list-item @click.native="right = !right">
+          <v-list-item-action>
+            <v-icon light>
+              mdi-repeat
+            </v-icon>
+          </v-list-item-action>
+          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-footer
+      :fixed="fixed"
+      app
+    >
+      <span>&copy; 2019</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import * as d3 from 'd3'
+export default {
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Stockmap'
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex
+      xs12
+      sm8
+      md6
+    >
+      <v-card>
+        <v-card-title class="headline">
+          Welcome to Stock Map!
+        </v-card-title>
+        <heatmap></heatmap>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import Heatmap from '../components/Heatmap.vue'
+
+export default {
+  data: () => ({
+  }),
+  components: {
+    'heatmap': Heatmap
+  }
+}
+</script>
